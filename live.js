71970db2ef68fe23a46bd146cbb9be5d8adf257a
Matthew Phillips <matthew@matthewphillips.info>
2015-04-20 17:10:25 -0400

    Some docs and delete earlier

--- live.js
+++ live.js
@@ -1,5 +1,18 @@
 var loader = require("@loader");
 
+/**
+ * A map of modules names to parents like:
+ * {
+ *   "child": {
+ *     "parentA": true,
+ *     "parentB": true
+ *   },
+ *   "parentA": false
+ * }
+ *
+ * This is used to recursively delete parent modules
+ *
+ */
 loader._liveMap = {};
 
 // Put a hook on `normalize` so we can keep a reverse map of modules to parents.
@@ -11,11 +24,14 @@ loader.normalize = function(name, parentName){
 	
 	if(!parentName) {
 		return done.then(function(name){
+			// We need to keep modules without parents to so we can know
+			// if they need to have their `onLiveReload` callbacks called.
 			loader._liveMap[name] = false;
 			return name;
 		});
 	}
 	
+	// Once we have the fully normalized module name mark who its parent is.
 	return done.then(function(name){
 		var parents = loader._liveMap[name];
 		if(!parents) {
@@ -29,46 +45,51 @@ loader.normalize = function(name, parentName){
 };
 
 // Teardown a module name by deleting it and all of its parent modules.
-function teardown(moduleName) {
+function teardown(moduleName, needsImport) {
 	var mod = loader.get(moduleName);
 	if(mod) {
+		loader.delete(moduleName);
+
 		var promise;
+		// If this module has a `modLiveReloadTeardown` function call it.
 		if(mod.liveReloadTeardown) {
 			promise = Promise.resolve(mod.liveReloadTeardown());
 		} else {
 			promise = Promise.resolve();
 		}
 		return promise.then(function(){
-			loader.delete(moduleName);
+			// Delete the module and call teardown on its parents as well.
 			var parents = loader._liveMap[moduleName];
-			if(!parents) return Promise.resolve(moduleName);
+			if(!parents) {
+				needsImport[moduleName] = true;
+				return Promise.resolve();
+			}
 
 			var promises = [];
 			for(var parentName in parents) {
-				promises.push(teardown(parentName));
+				promises.push(teardown(parentName, needsImport));
 			}
 			return Promise.all(promises);
 		});
 	}
-	return Promise.resolve(moduleName);
+	return Promise.resolve();
 }
 
 function reload(moduleName) {
-	teardown(moduleName).then(function(parents){
-		var imports = topLevelParents(parents).map(function(parentName){
-			return loader.import(parentName);
-		});
+	// Call teardown to recursively delete all parents, then call `import` on the
+	// top-level parents.
+	var parents = {};
+	teardown(moduleName, parents).then(function(){
+		var imports = [];
+		for(var parentName in parents) {
+			imports.push(loader.import(parentName));
+		}
+		// Once everything is imported call the `onLiveReload` callback functions.
 		Promise.all(imports).then(function(){
 			for(moduleName in loader._liveMap) {
 				handleReload(moduleName);	
 			}
 		});
-	});
-}
-
-function topLevelParents(parents){
-	return parents.filter(function(parentName){
-		return !loader._liveMap[parentName];
 	});
 }
 
